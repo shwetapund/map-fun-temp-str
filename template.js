6ea689aee6763a3788786cba1d391bc56b776465
shwetapund <pundshweta2020@gmail.com>
2022-12-18 00:43:11 -0800

    template string is added

---
+++ template.js
@@ -0,0 +1,7 @@
+
+const  studentName= "shweta"
+
+/* const message = studentName + " is " + studentAge + " years old " */
+
+const message = `Hello ${studentName}, Happy Diwali`
+console.log(message) 
